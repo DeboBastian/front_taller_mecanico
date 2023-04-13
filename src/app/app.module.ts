1380d8b2b36d3fbe9c9e7c30a0038a8ff785c04f
Irina Moyano García-Camba <irinamg9@gmail.com>
2023-04-13 22:33:37 +0200

    Fix login Component import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './components/users/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { MechanicComponent } from './components/mechanic/mechanic.component';
-import { LoginHomeAdministrateComponent } from './components/login-home-administrate/login-home-administrate.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { AdministrationComponent } from './components/administration/administration.component';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     LoginComponent,
     HomeComponent,
     MechanicComponent,
-    LoginHomeAdministrateComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdministrationComponent,
+    RegisterComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
