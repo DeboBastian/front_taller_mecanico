3b24a40824f23e356c1388e7f4201432377022f0
DeboBastian <cursosdeprogramacion00@gmail.com>
2023-04-13 18:08:46 +0200

    imports añadidos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginHomeAdministrateComponent } from './components/login-home-administrate/login-home-administrate.component';
+import { MechanicComponent } from './components/mechanic/mechanic.component';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "/home" },
-  { path: "home", component: Home },
+  { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
-  { path: "login/administration", component: Administration },
-  { path: "login/mechanics", component: Mechanics },
+  { path: "login/administration", component: LoginHomeAdministrateComponent },
+  { path: "login/mechanics", component: MechanicComponent },
   { path: "**", redirectTo: "/home" },
 ];
 
