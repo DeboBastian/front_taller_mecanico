af41478da8a43a1991620d06d5fc95f872108838
DeboBastian <cursosdeprogramacion00@gmail.com>
2023-04-13 17:23:41 +0200

    Rutas terminadas-pruebas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/home" },
+  { path: "home", component: Home },
+  { path: "login", component: LoginComponent },
+  { path: "login/administration", component: Administration },
+  { path: "login/mechanics", component: Mechanics },
+  { path: "**", redirectTo: "/home" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
