edf24d2d0a0bd023f783c84e4d7cdee95ee139d7
Selene González Blanco <sgonzalez2803@gmail.com>
2023-04-13 17:22:18 +0200

    commit component mechanic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MechanicComponent } from './components/mechanic/mechanic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MechanicComponent
   ],
   imports: [
     BrowserModule,
