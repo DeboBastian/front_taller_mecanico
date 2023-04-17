208e102e4705924825e303eac612f5664e88bfb7
Selene González Blanco <sgonzalez2803@gmail.com>
2023-04-17 17:46:04 +0200

    search mechanic

--- src/app/components/mechanic/mechanic.component.ts
+++ src/app/components/mechanic/mechanic.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Reparation } from 'src/app/interfaces/mechanic.interface';
+import { ReparationsService } from 'src/app/services/reparations.service';
 
 @Component({
   selector: 'mechanic',
@@ -8,9 +10,25 @@ import { FormControl } from '@angular/forms';
 })
 export class MechanicComponent {
 
+    //creo array series
+    arrReparations: Reparation[];
 
-  constructor() { }
+  constructor(private reparationsService: ReparationsService) { 
+        //inicializo array en el constructor
+        this.arrReparations = [];
+  }
 
-  
+  ngOnInit(){
+    this.arrReparations = this.reparationsService.getAll();
+  }
+
+  onChange($event: any) {
+    
+    //visualiza TODAS las reparaciones
+    if ($event.target.value === 'todos') {
+      this.arrReparations = this.reparationsService.getAll();
+    }
+
+}
 
 }
--- src/app/services/reparations.service.ts
+++ src/app/services/reparations.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Reparation } from '../interfaces/mechanic.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReparationsService {}
+export class ReparationsService {
+  
+  arrReparations = [];
+
+  constructor() {
+    this.arrReparations;
+  }
+  
+  //Me devuelve todo el array
+  getAll(): Reparation[] {
+    return this.arrReparations;
+  }
+
+}
